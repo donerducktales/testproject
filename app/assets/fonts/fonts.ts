7aa60dcaa456b183497ad5d9ea4cf66ae7e13a89
donerducktales <randomercook231@gmail.com>
2024-08-03 22:32:39 +0300

    додано новий шрифт в fonts.ts, внесені правки в footer.css, зроблено верхню секцію сторінки singlepost

--- app/assets/fonts/fonts.ts
+++ app/assets/fonts/fonts.ts
@@ -1,4 +1,4 @@
-import { Inter, Plus_Jakarta_Sans, Work_Sans } from "next/font/google";
+import { Inter, Plus_Jakarta_Sans, Source_Code_Pro, Work_Sans } from "next/font/google";
 
 export const work_sans = Work_Sans({
    subsets: ['latin'],
@@ -13,4 +13,10 @@ export const inter = Inter({
 export const plus_jakarta_sans = Plus_Jakarta_Sans({
    subsets: ['latin'],
    weight: ['400', '600', '800'],
+});
+
+export const source_serif_pro = Source_Code_Pro({
+   subsets: ['latin'],
+   weight: ['400'],
+   style: ['normal', 'italic'],
 });
--- app/singlepost/page.tsx
+++ app/singlepost/page.tsx
@@ -1,7 +1,35 @@
+import Image from 'next/image';
+import './singlepost.css';
+import userIcon from '@/public/img/singlepost/user-icon.png'
+import beautifulBuilding from '@/public/img/singlepost/beautiful-building.png'
+
 export default function SinglePost() {
    return (
       <main className="main">
-         singlepost
+         <section className="blog-info_section">
+            <div className="blog-info-section_container">
+               <div className="blog-info_topic">
+                  <p>Technology</p>
+               </div>
+               <h1 className="blog-info_header">
+                  The Impact of Technology on the Workplace: How Technology is Changing
+               </h1>
+               <div className="blog-info_user">
+                  <div className="blog-info-user_image">
+                     <Image 
+                        src={userIcon}
+                        alt='user icon'
+                        width={28}
+                        height={28}
+                     />
+                  </div>
+                  <div className="blog-info-user_data">
+                     <p style={{fontWeight: '500'}}>Tracey Wilson</p>
+                     <p style={{fontWeight: '400'}}>August 20, 2022</p>
+                  </div>
+               </div>
+            </div>
+         </section>
       </main>
    );
 }
